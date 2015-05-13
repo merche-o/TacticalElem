c11671d585402bcde7a02d809c05d9145d35eb8e
tigrou-j <joris.tigroudja@gmail.com>
2015-05-13 23:27:10 +0900

    --

--- Tactical_Element/Tactical_Element/Unit.cpp
+++ Tactical_Element/Tactical_Element/Unit.cpp
@@ -31,10 +31,14 @@ Unit *Unit::createWaterUnit(Unit *character, int nTeam, int nPlayer)
 	character->move_points = 5;
 	
 	//push les spells suivant le type dans la spell list du character
-	character->spells.push_back(new Spell(4, "toto1"));
-	character->spells.push_back(new Spell(3, "toto2"));
-	character->spells.push_back(new Spell(2, "toto3"));
+	character->spells.push_back(new Spell(2, 0, "Water 1 : Damage"));
+	character->spells.push_back(new Spell(2, 0, "Water 2 : Heal"));
+	character->spells.push_back(new Spell(2, 0, "Water 3 : Create a wall"));
 	
+	character->spells[0]->effect->setLife(-3);
+	character->spells[1]->effect->setLife(1);
+	character->spells[2]->effect->setWall(true);
+
 	return character;
 }
 
@@ -52,10 +56,14 @@ Unit *Unit::createLightningUnit(Unit *character, int nTeam, int nPlayer)
 	character->move_points = 5;
 	
 	//push les spells suivant le type dans la spell list du character
-	character->spells.push_back(new Spell(4, "toto1"));
-	character->spells.push_back(new Spell(4, "toto2"));
-	character->spells.push_back(new Spell(4, "toto3"));
+	character->spells.push_back(new Spell(2, 0, "L 1 : Damage"));
+	character->spells.push_back(new Spell(2, 0, "L 2 : Heal"));
+	character->spells.push_back(new Spell(2, 0, "L 3 : Create a wall"));
 	
+	character->spells[0]->effect->setLife(-3);
+	character->spells[1]->effect->setLife(1);
+	character->spells[2]->effect->setWall(true);
+
 	return character;
 }
 
@@ -71,9 +79,13 @@ Unit *Unit::createHeartUnit(Unit *character, int nTeam, int nPlayer)
 	character->move_points = 5;
 	
 	//push les spells suivant le type dans la spell list du character
-	character->spells.push_back(new Spell(4, "toto1"));
-	character->spells.push_back(new Spell(4, "toto2"));
-	character->spells.push_back(new Spell(4, "toto3"));
+	character->spells.push_back(new Spell(2, 0, "H 1 : Damage"));
+	character->spells.push_back(new Spell(2, 0, "H 2 : Heal"));
+	character->spells.push_back(new Spell(2, 0, "H 3 : Create a wall"));
+	
+	character->spells[0]->effect->setLife(-3);
+	character->spells[1]->effect->setLife(1);
+	character->spells[2]->effect->setWall(true);
 	
 	return character;
 }
